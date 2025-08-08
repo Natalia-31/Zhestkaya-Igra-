e804f86fc26bf47c50352db2d00e84cbe9d280ad
Natalia-31 <sedos.sedyakina21@gmail.com>
2025-08-08 16:03:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
 from config import BOT_TOKEN, ADMIN_IDS
-from handlers.game_handlers import register_game_handlers
-from handlers.admin_handlers import register_admin_handlers
+from game_handlers import register_game_handlers
+from admin_handlers import register_admin_handlers
 from database.models import init_db
 
 # Настройка логирования
