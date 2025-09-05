51a37d6a403d1a256b09cda664e39d8b84688df5
Natalia-31 <sedos.sedyakina21@gmail.com>
2025-09-05 16:16:25 +0300

    Update database_models.py

--- database_models.py
+++ database_models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = "sqlite:///database/game.db"
 
@@ -8,7 +8,6 @@ SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
-# Модель игрока
 class Player(Base):
     __tablename__ = "players"
     id = Column(Integer, primary_key=True, index=True)
@@ -16,12 +15,10 @@ class Player(Base):
     score = Column(Integer, default=0)
     is_leader = Column(Boolean, default=False)
 
-# Модель игры
 class Game(Base):
     __tablename__ = "games"
     id = Column(Integer, primary_key=True, index=True)
     current_round = Column(Integer, default=0)
 
-# Инициализация БД
 def init_db():
     Base.metadata.create_all(bind=engine)
