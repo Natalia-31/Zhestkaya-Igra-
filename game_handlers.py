e776dbbe24054f58e7137c4786a5565ea0b16e63
Natalia-31 <sedos.sedyakina21@gmail.com>
2025-09-05 14:31:40 +0300

    Update game_handlers.py

--- game_handlers.py
+++ game_handlers.py
@@ -1,8 +1,11 @@
-from aiogram import Dispatcher
+from aiogram import Router
 from aiogram.types import Message
 
+router = Router()
+
+@router.message(commands=["join"])
 async def handle_join(message: Message):
     await message.answer("➕ Вы присоединились к игре (заглушка).")
 
-def register_game_handlers(dp: Dispatcher):
-    dp.message.register(handle_join, commands={"join"})
+def register_game_handlers(dp):
+    dp.include_router(router)
