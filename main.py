e203a54b21c2d85ccab3fee33446bab1dd20e0a6
Natalia-31 <sedos.sedyakina21@gmail.com>
2025-09-15 10:27:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-# main.py
-
 import asyncio
 import logging
 from os import getenv
 
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
+from dotenv import load_dotenv
 
-from handlers import game_handlers
-from handlers import admin_handlers # <--- 1. ДОБАВЬТЕ ЭТУ СТРОКУ
-
-# ... (остальной код main.py) ...
+# Импортируем наши роутеры
+from handlers import game_handlers, admin_handlers
 
 async def main():
+    load_dotenv()
     bot = Bot(token=getenv("BOT_TOKEN"), parse_mode=ParseMode.HTML)
     dp = Dispatcher()
 
+    # Подключаем роутеры
+    dp.include_router(admin_handlers.router)
     dp.include_router(game_handlers.router)
-    dp.include_router(admin_handlers.router) # <--- 2. И ЭТУ СТРОКУ
-
+    
+    # Запускаем бота
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
-
