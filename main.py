54964634daf1aa908420753c49e72d46a2b131c8
Natalia-31 <sedos.sedyakina21@gmail.com>
2025-09-19 09:48:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,15 @@ from aiogram.client.default import DefaultBotProperties
 from handlers.game_handlers import router as game_router
 
 logging.basicConfig(level=logging.INFO)
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def main():
     if not BOT_TOKEN:
         raise RuntimeError("BOT_TOKEN не задан в переменных окружения")
 
-    bot = Bot(
-        token=BOT_TOKEN,
-        default=DefaultBotProperties(parse_mode=None)
-    )
+    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=None))
     dp = Dispatcher(storage=MemoryStorage())
 
-    # Роутеры
     dp.include_router(game_router)
 
     await dp.start_polling(bot)
